6b14cfb00c1af6c87f91c38038bad259c0b9bfc7
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2025-02-02 16:12:17 -0400

    added remote copy

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd.colorscheme("rose-pine")
+
+vim.g.clipboard = {
+  name = "osc52",
+  copy = {
+    ["+"] = function(lines)
+      local data = table.concat(lines, "\n")
+      local encoded = vim.fn.system("base64", data):gsub("\n", "")
+      local seq = string.format("\x1b]52;c;%s\x07", encoded)
+      vim.fn.chansend(vim.v.stderr, seq)
+    end,
+  },
+  paste = {
+    ["+"] = function()
+      return {}
+    end,
+  },
+}
